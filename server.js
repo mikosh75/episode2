244f90f2e439f6a594951954fa7a4d573cbdd482
mikosh <mikosh@gmail.com>
2011-10-11 23:44:59 +0200

    primo commit

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express'),
-    faye = require('faye');
+    faye = require('faye'),
+    port = process.ARGV[2] || '8000';
 
 var bayeux = new faye.NodeAdapter({
   mount:    '/faye',
@@ -7,15 +8,41 @@ var bayeux = new faye.NodeAdapter({
 });
 
 var app = express.createServer();
-app.configure(function () {
-  app.use(express.bodyDecoder());
-  app.use(express.staticProvider(__dirname + '/public'));
+
+app.configure(function(){
+	app.use(express.methodOverride());
+	app.use(express.bodyParser());
+	app.use(app.router);
+});
+
+app.configure('development', function(){
+	app.use(express.static(__dirname + '/public'));
+	app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+	var oneYear = 31557600000;
+	app.use(express.static(__dirname + '/public', { maxAge: oneYear }));
+	app.use(express.errorHandler());
 });
 
 app.post('/message', function(req, res) {
-  bayeux.getClient().publish('/channel', {text: req.body.message});
-  res.send(200);
+	bayeux.getClient().publish('/channel/1', { text1: req.body.inputfield, text2: req.body.timestamp });
+	res.send(200);
+});
+
+app.get('/', function(req, res){
+	  res.send('hello world');
+});
+
+app.get('/message', function(req, res){
+	  res.send('devi chiamare questa pagina con POST');
 });
 
 bayeux.attach(app);
-app.listen(8000);
+app.listen(Number(port));
+
+console.log('Listening on port ' + port );
+bayeux.getClient().subscribe('/channel/*', function(message) {
+				       console.log(new Date() + ' [' + message.text2 + ']: ' + message.text1);
+				       });
